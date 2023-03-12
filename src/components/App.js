13ae4da804ebb34196d4f1f07c63d85dda9a9a5c
SanjuManna <sanjumanna2201@gmail.com>
2023-03-12 21:17:11 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [timeStr, setTimeStr] = useState(new Date());
+
+  const interval = setInterval(() => {
+    setTimeStr(new Date());
+  }, 1000);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+        {timeStr.toLocaleDateString() + ", " + timeStr.toLocaleTimeString()}
+      </div>
     </div>
   )
 }
